d2889438fb6bd1aed90284644330ed38abdf5b4d
Tom Nielsen <tanielsen@gmail.com>
2020-08-18 19:50:25 +0100

    fit bounds

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const Table = require("@saltcorn/data/models/table");
 const Form = require("@saltcorn/data/models/form");
 const Field = require("@saltcorn/data/models/field");
 
-
 const headers = [
   {
-    script:
-      "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js",
-    integrity: "sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+    script: "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js",
+    integrity:
+      "sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
   },
   {
     css: "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css",
-    integrity:"sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+    integrity:
+      "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
   }
 ];
 
@@ -79,20 +79,22 @@ const configuration_workflow = () =>
                     .map(f => f.name)
                     .join()
                 }
-              },{
+              },
+              {
                 name: "height",
                 label: "Height in px",
                 type: "Integer",
-                required: true,   
-                default: 300                
+                required: true,
+                default: 300
               },
-              ,{
+              ,
+              {
                 name: "popup_width",
                 label: "Popup width in px",
                 type: "Integer",
-                required: true,   
-                default: 300                
-              },
+                required: true,
+                default: 300
+              }
             ]
           });
         }
@@ -100,55 +102,60 @@ const configuration_workflow = () =>
     ]
   });
 
-  const get_state_fields = async (table_id) => {
-    const table_fields = await Field.find({ table_id });
-    return table_fields.map(f => {
-      const sf = new Field(f);
-      sf.required = false;
-      return sf;
-    });
-  };
+const get_state_fields = async table_id => {
+  const table_fields = await Field.find({ table_id });
+  return table_fields.map(f => {
+    const sf = new Field(f);
+    sf.required = false;
+    return sf;
+  });
+};
 
 const run = async (
   table_id,
   viewname,
-  {
-    popup_view,
-    latitude_field,
-    longtitude_field,
-    height,
-    popup_width
-  },
+  { popup_view, latitude_field, longtitude_field, height, popup_width },
   state,
   extraArgs
 ) => {
-  const id=`map${Math.round(Math.random()*100000)}`
+  const id = `map${Math.round(Math.random() * 100000)}`;
   const popview = await View.findOne({ name: popup_view });
   if (!popview)
-  return div(
-    { class: "alert alert-danger" },
-    "Leaflet map incorrectly configured. Cannot find view: ",
-    popup_view
-  );
+    return div(
+      { class: "alert alert-danger" },
+      "Leaflet map incorrectly configured. Cannot find view: ",
+      popup_view
+    );
+
   const popresps = await popview.runMany(state, extraArgs);
-  
 
+  if (popresps.length === 0) return div("No locations");
 
-  const mkMarker = ({html, row}) => `
-  L.marker([${row[latitude_field]}, ${row[longtitude_field]}]).addTo(map)
-    .bindPopup(${JSON.stringify(html)}, {maxWidth: ${popup_width+10}, minWidth: ${popup_width-10}});
-  `
-
-  return div({id, style: `height:${height}px;`})+
-    script(domReady(`
-var map = L.map('${id}').setView([51.505, -0.09], 13);
+  const row0 = popresps[0].row;
+  const iniloc = `[${row0[latitude_field]}, ${row0[longtitude_field]}]`;
+  const the_data = popresps.map(({ html, row }) => [
+    [row[latitude_field], row[longtitude_field]],
+    html
+  ]);
+  return (
+    div({ id, style: `height:${height}px;` }) +
+    script(
+      domReady(`
+var points = ${JSON.stringify(the_data)};
+var map = L.map('${id}').setView(${iniloc}, 11);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-${popresps.map(mkMarker).join("\n")}
-    `));
-
-}
+points.forEach(pt=>{
+  L.marker(pt[0]).addTo(map)
+    .bindPopup(pt[1], {maxWidth: ${popup_width + 5}, minWidth: ${popup_width -
+        5}});
+});
+map.fitBounds(points.map(pt=>pt[0]))
+`)
+    )
+  );
+};
 module.exports = {
   sc_plugin_api_version: 1,
   headers,
@@ -158,7 +165,7 @@ module.exports = {
       display_state_form: false,
       get_state_fields,
       configuration_workflow,
-      run,
+      run
     }
   ]
 };
