90621ccd8a78826899a8415ceffb21600e21918e
Tom Nielsen <tanielsen@gmail.com>
2025-09-06 16:49:17 +0100

    onlyViews

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 const { features } = require("@saltcorn/data/db/state");
-const headers =
-  features && features.deep_public_plugin_serve
-    ? [
-        {
-          script: `/plugins/public/leaflet-map@${
-            require("./package.json").version
-          }/leaflet.js`,
-        },
-        {
-          css: `/plugins/public/leaflet-map@${
-            require("./package.json").version
-          }/leaflet.css`,
-        },
-      ]
-    : [
-        {
-          script: "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js",
-          integrity:
-            "sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==",
-        },
-        {
-          css: "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css",
-          integrity:
-            "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==",
-        },
-      ];
+const headers = [
+  {
+    script: `/plugins/public/leaflet-map@${
+      require("./package.json").version
+    }/leaflet.js`,
+    onlyViews: ["Leaflet map", "Leaflet map - multi-table"],
+  },
+  {
+    css: `/plugins/public/leaflet-map@${
+      require("./package.json").version
+    }/leaflet.css`,
+    onlyViews: ["Leaflet map", "Leaflet map - multi-table"],
+  },
+];
 
 module.exports = {
   sc_plugin_api_version: 1,
